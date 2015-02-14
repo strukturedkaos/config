b7199223b019fbd20a3bdbbc9a3e53ed9a5e34ae
Don Pottinger <don.pottinger@kevy.com>
2015-02-14 14:06:09 -0500

    Changes for Don

--- vim.symlink/ftdetect/spark.vim
+++
@@ -1,1 +0,0 @@
-au BufRead,BufNewFile *.spark set filetype=html
--- zsh.symlink/aliases.zsh
+++ zsh.symlink/aliases.zsh
@@ -1,25 +1,17 @@
 # My aliases
 alias b='cd -'
 alias cls='clear'
-alias sr='screen -r'
 alias restart='touch tmp/restart.txt'
-alias ajaxrdoc="rdoc --fmt ajax --exclude '.*generator.*' --exclude '.*test.*' --exclude '.*spec.*'"
 alias be='bundle exec'
 alias devlog='tail -200 -f log/development.log'
 alias testlog='tail -200 -f log/test.log'
-alias gvim='mvim -p'
 alias mysql_start='mysql.server start'
 alias mysql_stop='mysql.server stop'
-alias pg_start='pg_ctl -D ~/.pgdata -l ~/.pgdata/psql.log start'
-alias pg_stop='pg_ctl -D ~/.pgdata stop -s -m fast'
 alias redis_start='redis-server /usr/local/etc/redis.conf'
 
-function gitdays {
-  git log --author=Steven --reverse --since="$@ days ago" --pretty="format:%n%Cgreen%cd%n%n%s%n%b%n---------------------------------------------"
-}
+source '/usr/local/opt/chruby/share/chruby/chruby.sh'
+chruby 2.0.0-p594
+source '/usr/local/share/chruby/auto.sh'
 
-# Open a file in Marked.app. Usage: $ marked path/to/file.markdown
-function marked {
-  open -a Marked\ 2.app $@
-}
-
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
