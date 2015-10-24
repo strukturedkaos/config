ff7db5b232fdf25f79fb0d3180f67b8dbc1fed4d
Don Pottinger <don.pottinger@kevy.com>
2015-10-24 09:52:22 -0400

    Updates aliases

--- zsh.symlink/aliases.zsh
+++ zsh.symlink/aliases.zsh
@@ -8,10 +8,14 @@ alias testlog='tail -200 -f log/test.log'
 alias mysql_start='mysql.server start'
 alias mysql_stop='mysql.server stop'
 alias redis_start='redis-server /usr/local/etc/redis.conf'
+alias dmb='git branch --merged | grep -v "*" | xargs -n 1 git branch -d'
 
 source '/usr/local/opt/chruby/share/chruby/chruby.sh'
-chruby 2.0.0-p594
+chruby 2.1.6
 source '/usr/local/share/chruby/auto.sh'
 
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+alias gup='git smart-pull'
+alias clean='git branch --merged | grep -v "*" | xargs -n 1 git branch -d'
